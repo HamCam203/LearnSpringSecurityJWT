903a2cd45fb514fab0d3758f24c896d66c30a842
HamCam203 <camarahamoudia@gmail.com>
2025-02-25 15:05:26 +0100

    feat: Implement JWT decoding and token generation for secure API access
    - Added JWT decoding functionality to process incoming requests with Bearer tokens.
    - Implemented a new /login endpoint in LoginController to allow clients to obtain a JWT.
    - Created JWTService class with a generateToken() method to generate a JWT based on user authentication.
    - Configured Spring Security to provide a valid JwtEncoder instance.
    - Tested with Postman: successfully generated and validated JWT for secure resource access.
    - Next steps: Consider using asymmetric keys and adding a token scope for authorization control.

--- SpringSecurityJWT/src/main/java/com/openclassrooms/SpringSecurityJWT/configuration/SpringSecurityConfig.java
+++ SpringSecurityJWT/src/main/java/com/openclassrooms/SpringSecurityJWT/configuration/SpringSecurityConfig.java
@@ -14,9 +14,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 
 @Configuration
@@ -51,6 +55,11 @@ public class SpringSecurityConfig {
     public JwtDecoder jwtDecoder() {
         SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length,"RSA");
         return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+    }
+    
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
     }
 
 
